760192bd130a4148427182a7edff2ebdac849448
John David Ibañez <JhonhRge@gmail.com>
2018-11-01 08:21:06 -0500

    Update fileTranferBatExample.bat

--- fileTranferBatExample.bat
+++ fileTranferBatExample.bat
@@ -1,3 +1,6 @@
 @echo off
 echo hello
 copy /b C:\Users\rescate\Documents\CarpetaPRUEB\jaja.txt \\SISTEMAS70\Sistemas\Temp
+copy /b \\SISTEMAS70\Sistemas\Temp\Trayendoelarchivo.txt  C:\Users\rescate\Documents\CarpetaPRUEB
+pause > nul
+
