963c59ee58fff5ac04960d11834a5e922b694c22
CrkJohn <jhonhrge@gmail.com>
2018-11-19 08:44:00 -0500

    Create file and execute it

--- src/main/java/edu/eci/laboratorio/TranferenciaDeArchivos/Presentacion/TranferenciaDeArchivosGUI.java
+++ src/main/java/edu/eci/laboratorio/TranferenciaDeArchivos/Presentacion/TranferenciaDeArchivosGUI.java
@@ -5,8 +5,11 @@ import javax.swing.JFrame;
 import edu.eci.laboratorio.TranferenciaDeArchivos.entites.TransferenciaDeArhivosException;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.lang.Runtime;
 
 public class TranferenciaDeArchivosGUI extends JFrame{
@@ -16,53 +19,19 @@ public class TranferenciaDeArchivosGUI extends JFrame{
 	}
 	
 
-	public static void main(String[] args) throws TransferenciaDeArhivosException {
-		//TranferenciaDeArchivosGUI gui = new TranferenciaDeArchivosGUI();
-		//gui.setVisible(true);
+	public static void main(String[] args) throws TransferenciaDeArhivosException, IOException {
+		String url = "src\\main\\java\\edu\\eci\\laboratorio\\Archivos\\p.bat";
+		PrintWriter writer = new PrintWriter(url, "UTF-8");
 		
+		writer.println("@echo off");
+		writer.println("echo hello");
+		writer.println("");
 		
-			 		/*ProcessBuilder builder = new ProcessBuilder(
-			           "cmd.exe","c/", "pushd \\sistemas70\\Sistemas\\Temp","mkdir testo");
-			        builder.redirectErrorStream(true);
-			        try {
-						 Process p = builder.start();
-						 BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-					        String line;
-					        while (true) {
-					            line = r.readLine();
-					            if (line == null) { break; }
-					            System.out.println(line);
-					       }
-						System.err.println(p.isAlive());
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}*/
+		writer.close();
+		Runtime.getRuntime().exec("cmd /c start "+url+ " /V:OFF");
+		System.out.println("Se logro");
 		
-		try {
-			/*String can = "-Sistemas";
-			String arg = "cd \\\\sistemas70\\Sistemas\\Temp mkdir gordo1";
-			String ars = "cd Documents mkdir gordo1";
-			 */
-			String arg = "pushd \\\\sistemas70\\Sistemas\\Temp";
-			//;
-			Runtime rt = Runtime.getRuntime();
-			rt.exec("cmd.exe /c cd \\Users\\rescate  & start  cmd.exe /k \"java -flag -flag -cp terminal-based-program.jar\"");
-			//ProcessBuilder pbBuilder = new ProcessBuilder();
-			//pbBuilder.command(new String[]{"cmd.exe c/ notepad.exe"});
-			//pbBuilder.start();
-		
-			System.out.println("");
-			//String pruebas[] = new String[] {"pushd "+arg+" && mkdir gordo1"};
-			//Process builder = Runtime.getRuntime().exec(ars);
 
-		
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-			       
-			
 		
 	}
  
