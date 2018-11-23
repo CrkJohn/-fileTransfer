6aab1258e5bd6e30b1ca8884ee02cfcdc98b3573
CrkJohn <jhonhrge@gmail.com>
2018-11-22 22:01:57 -0500

    BackIn-70%,Falta Crear tables

--- fileTranferBatExample.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo hello
-copy /b C:\Users\rescate\Documents\CarpetaPRUEB\jaja.txt \\SISTEMAS70\Sistemas\Temp
-copy /b \\SISTEMAS70\Sistemas\Temp\Trayendoelarchivo.txt  C:\Users\rescate\Documents\CarpetaPRUEB
-pause > nul
-
